979a04172f4180361ed1a4178b3d35286d05e788
Mahdi Shokri <mahdi.shokri@specific-group.com>
2023-11-03 03:37:48 +0330

    second

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ from selenium.webdriver.common.proxy import Proxy, ProxyType
 absolute_path = os.path.dirname(__file__)
 relative_path = "index.html"
 full_path = os.path.join(absolute_path, relative_path)
+driver_path = os.path.join(absolute_path, 'chromedriver')
 print(full_path)
 
 class BankTest:
@@ -39,16 +40,20 @@ class BankTest:
         bank_test_load = (By.XPATH, "// h2[contains(text(), 'درگاه تست')]")
         bank_test_code = (By.XPATH, "//code")
         submit_form = (By.XPATH, "//*[@type='submit']")
-        self.browser = webdriver.Chrome(executable_path="/root/base/eways/chromedriver", options=self.options)
+        self.browser = webdriver.Chrome(executable_path=driver_path, options=self.options)
         # self.browser = webdriver.Chrome(options=self.options)
         self.browser.get(self.url)
         self.browser.get_screenshot_as_file("test.png")
         print(ref_id)
         self.browser.execute_script(f"document.getElementById('RefId').value = '{ref_id}'")
         # sleep(100)
+        page_source = self.browser.page_source
+        # Print the page source
+        print(page_source)
         self.wait_until_element_available(submit_form).click()
         self.wait_until_element_available(bank_test_load)
         # sleep(100)
+
         self.browser.find_element(By.XPATH, "//*[@type='submit']").click()
         sleep(1)
         response_code = self.browser.find_element(*bank_test_code).text.replace('"CardHolderPan"', ',"CardHolderPan"')
